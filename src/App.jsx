165877de2a2378d915505c3e2423744a51558bae
Swapnil Gandhale <swapnilgandhale08@gmail.com>
2025-07-31 13:42:19 +0530

    done with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="relative flex min-h-40 w-300 rounded-2xl flex-col items-center justify-center bg-gray-100 dark:bg-gray-900" >
+       <h1 className="text-xl font-bold underline">
+      Password Generator
+    </h1>
+
+       </div>
     </>
   )
 }
